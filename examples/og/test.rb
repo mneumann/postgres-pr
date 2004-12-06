820bd74c41b3e22e17a026a732b21bdcf21debe7
Michael Neumann <mneumann@ntecs.de>
2004-12-06 23:43:20 +0000

    Og/Nitro examples

---
+++ examples/og/test.rb
@@ -0,0 +1,50 @@
+$LOAD_PATH.unshift '../../lib'
+$LOAD_PATH.unshift '/tmp/og-0.5.0/lib'
+require 'og'
+require 'glue/logger'
+
+$DBG = true
+
+class User
+end
+
+class Comment
+  prop_accessor :body, String
+  belongs_to :user, User
+end
+
+class User
+  prop_accessor :name, String
+  has_many :comments, Comment
+end
+
+if __FILE__ == $0
+  config = {
+    :address => "localhost",
+    :database => "mneumann",
+    :backend => "psql",
+    :user => "mneumann",
+    :password => "",
+    :connection_count => 1
+  }
+  $log = Logger.new(STDERR)
+  $og = Og::Database.new(config)
+
+  $og.get_connection
+
+  u1 = User.new
+  u1.name = "Michael Neumann"
+  u1.save!
+
+  u2 = User.new
+  u2.name = "John User"
+  u2.save!
+
+  c1 = Comment.new
+  c1.body = "og test"
+  c1.user = u1
+  c1.save!
+
+  p User.all
+  p Comment.all
+end
